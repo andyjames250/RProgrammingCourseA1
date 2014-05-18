c60d0d4a418b52b4f0cd9154bf0363afe13b8326
andyjames250 <andyjames250@gmail.com>
2014-05-18 16:31:30 +1000

    Finished complete function

---
+++ complete.R
@@ -0,0 +1,25 @@
+complete <- function(directory, id = 1:332) {
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+        
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+        
+        ## Return a data frame of the form:
+        ## id nobs
+        ## 1  117
+        ## 2  1041
+        ## ...
+        ## where 'id' is the monitor ID number and 'nobs' is the
+        ## number of complete cases
+        filenames <- paste(formatC(id, width=3, format="d", flag="0"), ".csv", sep = "")
+        result <- NULL
+        for(i in 1:length(filenames)) {
+                data <- read.csv(paste(directory, filenames[i], sep = "/"))
+                data$completecase <- complete.cases(data)
+                result <- rbind(result, cbind.data.frame(id[i], sum(data$completecase)))
+        }
+        complete <- result
+        names(complete) <- c("id", "nobs")
+        complete
+}
